6aa8a9c89203a5be0a2d065061f363b154d3036f
lut <83720412+LinusUsernameTips@users.noreply.github.com>
2021-07-08 22:17:52 -0400

    Update webhook_spammer.py

--- webhook_spammer.py
+++ webhook_spammer.py
@@ -3,6 +3,10 @@ import colorama
 from colorama import Fore
 print(Fore.CYAN + "LUTS Webhook Spammer (Cntrl C to stop)")
 webhook = input(Fore.CYAN+ 'Paste webhook here : ')
+req = requests.get(webhook)
+if "Unknown Webhook" in req.text:
+  print(Fore.RED + 'Invalid Webhook.')
+  input()
 message = input(Fore.CYAN+ 'What message to spam? : ')
 username = input(Fore.CYAN+ 'Input the bots desired username : ')
 data = {"content": message,
@@ -13,9 +17,11 @@ try:
     r = requests.post(webhook, data=data)
     if 'rate limited' in r.text:
       print(Fore.RED + 'Error, Ratelimited.')
-    else:
+    elif r.text == '':
       message_amount += 1
       print(Fore.GREEN + f"{message_amount} Messages Sent!")
+    else:
+      print(Fore.YELLOW + "Error sending message.")
 except KeyboardInterrupt:
   print(Fore.CYAN + f'Stopping spam, {message_amount} messages sent.')
   input()
